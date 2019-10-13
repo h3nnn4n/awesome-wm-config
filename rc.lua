b8db57b3582bf821be8c3661f3dc66b335615e3e
h3nnn4n <uber.renan@gmail.com>
2019-10-13 18:20:24 -0300

    Switch back to urxvt

--- rc.lua
+++ rc.lua
@@ -48,7 +48,7 @@ end
 beautiful.init("/home/h3nnn4n/.config/awesome/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "xterm -bg black -fg grey"
+terminal = "urxvt"
 editor = os.getenv("EDITOR") or "nano"
 editor_cmd = terminal .. " -e " .. editor
 
